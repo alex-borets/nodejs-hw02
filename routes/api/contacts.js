ee8ab228ed2b37e0822438ced223478f80371deb
Alex Borets <alexborets2812@gmail.com>
2022-10-09 22:53:32 +0300

    hw-4 rdy

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,17 +2,18 @@ const express = require('express');
 
 const ctrl = require('../../controllers/contacts');
 const { ctrlWrapper } = require('../../helpers');
-const { isValidId, validateBody } = require('../../middlewares');
+const { isValidId, validateBody, authenticate } = require('../../middlewares');
 const { schemas } = require('../../models/contact');
 const router = express.Router();
 
-router.get('/', ctrlWrapper(ctrl.getAll));
-router.get('/:contactId', isValidId, ctrlWrapper(ctrl.getContactById));
-router.post('/', validateBody(schemas.addSchema), ctrlWrapper(ctrl.addContact));
-router.delete('/:contactId', isValidId, ctrlWrapper(ctrl.removeContact));
+router.get('/', authenticate, ctrlWrapper(ctrl.getAll));
+router.get('/:contactId', authenticate, isValidId, ctrlWrapper(ctrl.getContactById));
+router.post('/', authenticate, validateBody(schemas.addSchema), ctrlWrapper(ctrl.addContact));
+router.delete('/:contactId', authenticate, isValidId, ctrlWrapper(ctrl.removeContact));
 
 router.put(
   '/:contactId',
+  authenticate,
   isValidId,
   validateBody(schemas.addSchema),
   ctrlWrapper(ctrl.updateContact),
@@ -20,6 +21,7 @@ router.put(
 
 router.patch(
   '/:contactId/favorite',
+  authenticate,
   isValidId,
   validateBody(schemas.updateFavoriteSchema),
   ctrlWrapper(ctrl.updateFavoriteContact),
